ba035c21367254f54e32ef05daebc4088d56bb87
Dean Wan <deanwan14@gmail.com>
2024-08-10 22:56:53 +1000

    feat: add main func

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	svr := http.NewServeMux()
+
+	svr.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello there!")
+	})
+
+	fmt.Println("starting server on localhost at port 3000...")
+	http.ListenAndServe(":3000", svr)
 }
