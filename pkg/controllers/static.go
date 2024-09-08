2db6d373ab920f2150e347ae53b40fc633728ee3
Dean Wan <deanwan14@gmail.com>
2024-09-08 21:04:07 +1000

    doc: update method descriptions

--- pkg/controllers/static.go
+++ pkg/controllers/static.go
@@ -8,7 +8,7 @@ import (
 
 /*
 StaticHandler writes the data in a views.Template type to
-an io.ResponseWriter interface
+an io.ResponseWriter interface.
 */
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -17,12 +17,22 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+/*
+The Server type encapsulates a multiplexer (using *chi.Mux)
+and a standard http.Server type available in the standard library.
+*/
 type Server struct {
 	Mux    *chi.Mux
 	addr   string
 	server *http.Server
 }
 
+/*
+New creates a pointer to a Server type. It encapsulates the port
+on which the server will listenAndServe, as well as the *chi.Mux
+router. It sets some sensible defaults for timeouts and mounts
+handlers to the router.
+*/
 func New(ctx context.Context, port string) (*Server, error) {
 	port = ":" + port
 	mux := chi.NewRouter()
@@ -47,6 +57,11 @@ func New(ctx context.Context, port string) (*Server, error) {
 	return s, nil
 }
 
+/*
+Start calls ListenAndServe on the http.Server in the Server type,
+allowing it to start traffic. A failure on ListenAndServe that is not
+a http.ErrServerClosed (i.e. a server shutdown) will result in a panic.
+*/
 func (s *Server) Start(ctx context.Context) error {
 	log.Printf("starting server on localhost at port %s...", s.addr)
 
@@ -57,12 +72,21 @@ func (s *Server) Start(ctx context.Context) error {
 	return nil
 }
 
+/*
+Shutdown calls Shutdown on the http.Server embedded within the Server type (i.e.
+a graceful shutdown of the server).
+*/
 func (s *Server) Shutdown(ctx context.Context) error {
 	log.Printf("Shutting down server at address %s", s.addr)
 
 	return s.server.Shutdown(ctx)
 }
 
+/*
+mount registers various routes on the *chi.Mux router. It parses various templates
+that are embedded in the binary via fs.FS and uses HandlerFuncs provided in the
+`controllers` package to write these to io.ResponseWriter.
+*/
 func (s *Server) mount() {
 	log.Print("registering routes on server")
 
